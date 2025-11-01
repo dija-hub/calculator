64fb31429076d2f24f282c7973a63bf7188b9085
dija <dija5408@gmail.com>
2025-11-01 18:47:10 +0500

    practice

--- cal.js
+++ cal.js
@@ -1,3 +1,15 @@
-let ac=document.getElementById("ac");
+let one=document.getElementById("one");
+let two=document.getElementById("two")
+let add=document.getElementById("add");
+let oper=document.getElementById("oper");
+let num=document.getElementsByClassName("num")
 
+let display=document.getElementById("display")
 
+num.addEventListener("click",()=>{
+      console.log(num.innerText)
+})
+function sum (num,num){
+console.log(num+num);
+}
+sum(one,two)
